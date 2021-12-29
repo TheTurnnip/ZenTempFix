f7ddc9f0a505acd492b968269e5a23f479c7c234
TheTurnnip <ryan.steffan20@gmail.com>
2021-12-29 15:43:55 +0000

    Updates to add licence

--- zentempfix.sh
+++ zentempfix.sh
@@ -83,3 +83,19 @@ gentask
 enabler
 
 echo "Script has compleated install"
+
+#ZenTempFix, A script to fix tempature sensors on the 1.15 Linux Kernel
+#Copyright (C) 2021  Ryan Steffan
+
+#This program is free software: you can redistribute it and/or modify
+#it under the terms of the GNU General Public License as published by
+#the Free Software Foundation, either version 3 of the License, or
+#(at your option) any later version.
+
+#This program is distributed in the hope that it will be useful,
+#but WITHOUT ANY WARRANTY; without even the implied warranty of
+#MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#GNU General Public License for more details.
+
+#You should have received a copy of the GNU General Public License
+#along with this program. If not, see <https://www.gnu.org/licenses/>.
